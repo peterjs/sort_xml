8e4eb8b320199cda7d504d891309fcffdbc30d61
ludovitsiket <siket.ludovit@gmail.com>
2016-09-21 14:36:40 +0200

    triedenie xml suborov do spravnej zlozky

--- sort_xml.py
+++ sort_xml.py
@@ -13,7 +13,23 @@ def check_xml(file):
 def read_xml():
   return
   
+def get_value_from_xml_file(xml_file, values):
+  found_value = ""
+  f = open(xml_file, 'r')
+  lines = f.read()
+  for value in values:
+    if lines.find(value) != -1:
+      found_value = value
+  f.close()
+  return found_value
+  
+def get_name_from_xml(xml_file):
+  return get_value_from_xml_file(xml_file, ["HESS"])
+
 def get_sr_kod_from_xml(xml_file):
+  return get_value_from_xml_file(xml_file, ["P21697063201","P21697208301"])
+  
+def get_sr_kod_from_xml2(xml_file):
   sr_kod = ""
   f = open(xml_file, 'r')
   lines = f.read()
@@ -21,6 +37,7 @@ def get_sr_kod_from_xml(xml_file):
     sr_kod = "P21697063201"
   elif lines.find("P21697208301") != -1:
     sr_kod = "P21697208301"
+  f.close()
   return sr_kod
 
 def copy_file_to_dir(vstup_subor, vystup_zlozka):
@@ -40,10 +57,11 @@ def main():
       full_path_to_file = os.path.join(source_directory,xml_file)
       print(full_path_to_file)
       sr_kod = get_sr_kod_from_xml(full_path_to_file)
+      name = get_name_from_xml(full_path_to_file)
       print(sr_kod)
-      if sr_kod == "P21697208301":
+      if sr_kod == "P21697208301" and name == "HESS":
         copy_file_to_dir(full_path_to_file, dial_directory)        
-      elif sr_kod == "P21697063201":
+      elif sr_kod == "P21697063201" and name == "HESS":
         copy_file_to_dir(full_path_to_file, amb_directory)
       else:
         log("log.txt","chybny sr_kod " + str(sr_kod) + " v subore " + str(xml_file) +"\n")
