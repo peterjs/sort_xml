01034927ed8e1ca2d5363c123c8b25f89cf941aa
Peter Somlo <peter.somlo@gmail.com>
2017-05-16 07:58:14 +0200

    funkcia na pridanie hodnoty atributu xml tagu

--- sort_xml.py
+++ sort_xml.py
@@ -1,6 +1,10 @@
 # coding=utf-8
 import os, fnmatch, shutil,sys
 from time import localtime, strftime
+import xml.etree.ElementTree as ET
+
+id_lis_map = {"HGB": "99901", "HTC": "99902", "WBC": "99903", "PLT": "99904", "RET%": "99905", "S-GLU": "01898", "S-UREA": "03085", "S-KREA": "01511", "S-KM": "99910", "S-CB": "99911", "S-ALB": "00504", "S-TBIL": "01153", "S-Fe": "99921", "S-TIBC": "99922", "S-FeSat": "99923", "S-FER": "99924", "S-Na": "99925", "S-K": "99926", "S-Ca": "99927", "S-Mg": "99928", "S-Cl": "99929", "S-P": "99930", "S-CRP hs": "99931", "S-vit.D": "99934", "U-pH": "99802", "U-BIEL": "99803", "U-GLUK": "99804", "U-KETO": "99805", "U-KRV": "99806", "U-BILI": "99807", "U-UBG": "99808", "U-ERY": "99812", "U-LEUK": "99813", "U-ValHYAL": "99814", "U-BAKT": "99815", "U-EPIguľ": "99816"}
+
 
 def xmls_in_dir(directory):
   real_path = os.path.realpath(directory)
@@ -15,6 +19,7 @@ def check_xml(file):
 def read_xml():
   return
   
+
 def get_value_from_xml_file(xml_file, values):
   found_value = ""
   f = open(xml_file, 'r', encoding="cp1250")
@@ -59,13 +64,16 @@ def main():
       name = get_name_from_xml(full_path_to_file)
 #      print(sr_kod)
       if sr_kod == "P21697208301":
-        copy_file_to_dir(full_path_to_file, dial_directory)        
+        copy_file_to_dir(full_path_to_file, dial_directory)
+        modify_lab_test_codes(os.path.join(dial_directory,xml_file))
       elif sr_kod == "P21697063201":
         copy_file_to_dir(full_path_to_file, amb_directory)
+        modify_lab_test_codes(os.path.join(amb_directory,xml_file))
       elif sr_kod == "P21697063203":
         copy_file_to_dir(full_path_to_file, amb2_directory)
+        modify_lab_test_codes(os.path.join(amb2_directory,xml_file))
       else:
         log("log.txt", xml_file)
   return
-  
+
 main()
