65b4a7c46768ca5b34fbf6f5ee7fd6f2bbc7bd6d
Peter Somlo <peter.somlo@gmail.com>
2017-06-06 10:20:41 +0200

    upraveny zoznam mapovanych mien vysetreni na id - pridane medzery k niektorym nazvom

--- sort_xml.py
+++ sort_xml.py
@@ -3,7 +3,7 @@ import os, fnmatch, shutil,sys
 from time import localtime, strftime
 import xml.etree.ElementTree as ET
 
-id_lis_map = {"HGB": "99901", "HTC": "99902", "WBC": "99903", "PLT": "99904", "RET%": "99905", "S-GLU": "01898", "S-UREA": "03085", "S-KREA": "01511", "S-KM": "99910", "S-CB": "99911", "S-ALB": "00504", "S-TBIL": "01153", "S-Fe": "99921", "S-TIBC": "99922", "S-FeSat": "99923", "S-FER": "99924", "S-Na": "99925", "S-K": "99926", "S-Ca": "99927", "S-Mg": "99928", "S-Cl": "99929", "S-P": "99930", "S-CRP hs": "99931", "S-vit.D": "99934", "U-pH": "99802", "U-BIEL": "99803", "U-GLUK": "99804", "U-KETO": "99805", "U-KRV": "99806", "U-BILI": "99807", "U-UBG": "99808", "U-ERY": "99812", "U-LEUK": "99813", "U-ValHYAL": "99814", "U-BAKT": "99815", "U-EPIguľ": "99816"}
+id_lis_map = {"HGB": "99901", "HTC": "99902", "WBC": "99903", "PLT": "99904", "RET%": "99905", "S-GLU": "01898", "S-UREA": "03085", "S-KREA": "01511", "S-KM ": "99910", "S-CB ": "99911", "S-ALB": "00504", "S-TBIL": "01153", "S-Fe": "99921", "S-TIBC": "99922", "S-FeSat": "99923", "S-FER": "99924", "S-Na": "99925", "S-K": "99926", "S-Ca": "99927", "S-Mg": "99928", "S-Cl": "99929", "S-P": "99930", "S-CRP hs": "99931", "S-vit.D": "99934", "U-pH": "99802", "U-BIEL": "99803", "U-GLUK": "99804", "U-KETO": "99805", "U-KRV": "99806", "U-BILI": "99807", "U-UBG": "99808", "U-ERY": "99812", "U-LEUK": "99813", "U-ValHYAL": "99814", "U-BAKT": "99815", "U-EPIguľ": "99816"}
 
 
 def xmls_in_dir(directory):
